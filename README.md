a3dd28bb3c33506eb5cc1f5674ae8837b9e7bc74
John E. Vincent <lusis.org+github.com@gmail.com>
2014-11-27 03:03:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# httpclient
+`httpclient` is a unified wrapper around a few common http/s libraries and methods.
+
+The use case for this was being able to use an internal Github API client via openresty/`ngx.location.capture` and also outside of openresty using `luasocket` and `luasec`.
+
+The `luasocket.http` API is a bit different than the ngx api. This provides a "familiar" interface to both.
+
+*NOTE*: the wrappers for `ngx.location.capture` and `lua-resty-http` modules are not yet done. "Release early/release often" or something like that...
+
+## Usage
+You can look at the tests for various usage examples but most interactions use a common pattern.
+The result of any call will be a table with the following structure:
+
+```lua
+{
+  body = <response body>,
+  code = <http status code>,
+  headers = <table of headers>,
+  status_line = <the http status message>,
+  err = <nil or error message>
+}
+```
+
+### Get
+```lua
+hc = require('httpclient.luasocket').new()
+res = hc:get('http://httpbin.org/get')
+if res.body then
+  print(res.body)
+else
+  print(res.err)
+end
+-- {
+--   "args": {}, 
+--   "headers": {
+--     "Accept": "*/*", 
+--     "Connect-Time": "1", 
+--     "Connection": "close", 
+--     "Host": "httpbin.org", 
+--     "Total-Route-Time": "0", 
+--     "User-Agent": "LuaSocket 3.0-rc1", 
+--     "Via": "1.1 vegur", 
+--     "X-Request-Id": "b64c0bb9-653a-44ec-8bd2-a768ad80d720"
+--   }, 
+--   "origin": "1.1.1.1", 
+--   "url": "http://httpbin.org/get"
+-- }
+```
+
+### Post
+```lua
+hc = require('httpclient.luasocket').new()
+res = hc:post('http://httpbin.org/post','somepostdata')
+```
+
+The following verbs are supported:
+- `GET`
+- `PUT`
+- `POST`
+- `HEAD`
+- `PATCH`
+- `DELETE`
+
+Note that this library does not do any special handling of the response body other than giving it to you as-is.
+This library is intended to be used by a higher-level library that handles parsing
+
+## Other bits
+There are ways to override much of what you pass in to the actual http request specific to the driver.
+
+## Requirements
+The following versions were tested
+- lua 5.2 (the version that shipped with trusty)
+- luasec 0.5-2 (only required for https support)
+- luasocket 3.0rc1-1
+
+If you want to run the test suite:
+- luacov 0.6-1
+luaunit is included in the test dir
+
+## TODO
+- Add remaining drivers
+- make a rockspec/post to luarocks
+- Document better
---
+++ tests/luasocket.lua
@@ -0,0 +1,229 @@
+package.path = package.path..';tests/?.lua'
+require('luaunit')
+
+local httpclient = require("httpclient.luasocket")
+local cjson = require("cjson")
+require("luacov")
+
+TestHttpClientLuaSocket = {}
+
+function TestHttpClientLuaSocket:setup()
+end
+
+function TestHttpClientLuaSocket:test_failure()
+  local hc = httpclient.new()
+  local res = hc:get("http://nohost")
+  assertEquals(res.body, nil)
+  assertEquals(res.err, "host or service not provided, or not known")
+end
+
+function TestHttpClientLuaSocket:test_defaults()
+  local hc = httpclient.new()
+  assertEquals(type(hc:get_defaults()), "table")
+  -- existing key
+  hc:set_default("timeout", 120)
+  assertEquals(hc.defaults.timeout, 120)
+  assertEquals(hc:get_defaults().timeout, 120)
+  -- ssl opts
+  assertEquals(hc.defaults.ssl_opts.verify, "peer")
+  hc:set_default("ssl_opts", { verify = "none" })
+  assertEquals(hc.defaults.ssl_opts.verify, "none")
+  -- new key
+  hc:set_default("foo", "bar")
+  assertEquals(hc.defaults.foo, "bar")
+  assertEquals(hc:get_defaults().foo, "bar")
+  -- passed in via request is handled in timeout test
+end
+
+function TestHttpClientLuaSocket:test_query_table()
+  local params = {foo = "bar"}
+  local opts = {params = params}
+  local hc = httpclient.new()
+  local res = hc:get("http://httpbin.org/get",opts)
+  local b = cjson.decode(res.body)
+  assertEquals(b.args.foo, "bar")
+end
+
+function TestHttpClientLuaSocket:test_query_string()
+  local params = "foo=bar"
+  local opts = {params = params}
+  local hc = httpclient.new()
+  local res = hc:get("http://httpbin.org/get",opts)
+  local b = cjson.decode(res.body)
+  assertEquals(b.args.foo, "bar")
+end
+
+function TestHttpClientLuaSocket:test_timeout()
+  local hc = httpclient.new({timeout = 5})
+  assertEquals(hc.defaults.timeout, 5)
+  local res = hc:get("http://httpbin.org/delay/10")
+  assertEquals(res.body, nil)
+  assertEquals("timeout", res.err)
+end
+
+function TestHttpClientLuaSocket:test_head()
+  local hc = httpclient.new()
+  local res = hc:head("http://httpbin.org/get")
+  assertEquals(res.code, 200)
+  assertEquals(res.body, "")
+  assertEquals(res.headers["content-type"], "application/json")
+end
+
+function TestHttpClientLuaSocket:test_get()
+  local hc = httpclient.new()
+  local res = hc:get("http://httpbin.org/get")
+  -- assert we have all the bits
+  assertEquals(res.err, nil)
+  assertNotEquals(res.body, nil)
+  assertNotEquals(res.code, nil)
+  assertNotEquals(res.headers, nil)
+  assertNotEquals(res.status_line, nil)
+  -- now we test the actual data
+  local b = cjson.decode(res.body)
+  assertEquals(res.code, 200)
+  assertEquals(b.url, "http://httpbin.org/get")
+  assertEquals(res.headers["content-type"], "application/json")
+  assertEquals(res.status_line, "HTTP/1.1 200 OK")
+end
+
+function TestHttpClientLuaSocket:test_post()
+  local post_data = "foobarbang"
+  local hc = httpclient.new()
+  local res = hc:post("http://httpbin.org/post",post_data)
+  -- assert we have all the bits
+  assertEquals(res.err, nil)
+  assertNotEquals(res.body, nil)
+  assertNotEquals(res.code, nil)
+  assertNotEquals(res.headers, nil)
+  assertNotEquals(res.status_line, nil)
+  -- now we test the actual data
+  local b = cjson.decode(res.body)
+  assertEquals(res.code, 200)
+  assertEquals(b.data, post_data)
+  assertEquals(res.headers["content-type"], "application/json")
+  assertEquals(res.status_line, "HTTP/1.1 200 OK")
+end
+
+function TestHttpClientLuaSocket:test_put()
+  local put_data = "foobarbang"
+  local hc = httpclient.new()
+  local res = hc:put("http://httpbin.org/put",put_data)
+  -- assert we have all the bits
+  assertEquals(res.err, nil)
+  assertNotEquals(res.body, nil)
+  assertNotEquals(res.code, nil)
+  assertNotEquals(res.headers, nil)
+  assertNotEquals(res.status_line, nil)
+  -- now we test the actual data
+  local b = cjson.decode(res.body)
+  assertEquals(res.code, 200)
+  assertEquals(b.data, put_data)
+  assertEquals(res.headers["content-type"], "application/json")
+  assertEquals(res.status_line, "HTTP/1.1 200 OK")
+end
+
+function TestHttpClientLuaSocket:test_patch()
+  local patch_data = "foobarbang"
+  local hc = httpclient.new()
+  local res = hc:patch("http://httpbin.org/patch",patch_data)
+  -- assert we have all the bits
+  assertEquals(res.err, nil)
+  assertNotEquals(res.body, nil)
+  assertNotEquals(res.code, nil)
+  assertNotEquals(res.headers, nil)
+  assertNotEquals(res.status_line, nil)
+  -- now we test the actual data
+  local b = cjson.decode(res.body)
+  assertEquals(res.code, 200)
+  assertEquals(b.data, patch_data)
+  assertEquals(res.headers["content-type"], "application/json")
+  assertEquals(res.status_line, "HTTP/1.1 200 OK")
+end
+
+function TestHttpClientLuaSocket:test_delete()
+  local hc = httpclient.new()
+  local res = hc:delete("http://httpbin.org/status/204")
+  assertEquals(res.err, nil)
+  assertEquals(res.body, "")
+  assertNotEquals(res.code, nil)
+  assertNotEquals(res.headers, nil)
+  assertNotEquals(res.status_line, nil)
+  -- now we test the actual data
+  assertEquals(res.code, 204)
+  assertEquals(res.headers["content-length"], "0")
+  assertEquals(res.status_line, "HTTP/1.1 204 NO CONTENT")
+end
+
+function TestHttpClientLuaSocket:test_set_content_type()
+  local hc = httpclient.new()
+  local res = hc:post("http://httpbin.org/post","foobarbang",{content_type = "text/xml"})
+  local b = cjson.decode(res.body)
+  assertEquals(b.headers["Content-Type"], "text/xml")
+end
+
+function TestHttpClientLuaSocket:test_override_header()
+  local hc = httpclient.new()
+  local res = hc:get("http://httpbin.org/get",{headers = {accept = "application/json"}})
+  local b = cjson.decode(res.body)
+  assertEquals(b.headers["Accept"], "application/json")
+end
+
+function TestHttpClientLuaSocket:test_https()
+  local hc = httpclient.new()
+  local res = hc:get("https://httpbin.org/get")
+  assertEquals(res.code, 200)
+end
+
+function TestHttpClientLuaSocket:test_ssl_redirect()
+  local redir_to = "http://httpbin.org/get"
+  local hc = httpclient.new()
+  local res = hc:get("https://httpbin.org/redirect-to?url="..redir_to)
+  assertEquals(res.code, 200)
+  local b = cjson.decode(res.body)
+  assertEquals(b.url, redir_to)
+end
+
+function TestHttpClientLuaSocket:test_4xx()
+  local hc = httpclient.new()
+  local res = hc:get("http://httpbin.org/status/418")
+  assertEquals(res.code, 418)
+  assertNotEquals(res.body, nil)
+  assertNotEquals(res.headers, nil)
+  assertEquals(res.status_line, "HTTP/1.1 418 I'M A TEAPOT")
+  -- We don't care what the error IS just that it sets err
+  assertNotEquals(res.err, nil)
+end
+
+function TestHttpClientLuaSocket:test_5xx()
+  local hc = httpclient.new()
+  local res = hc:get("http://httpbin.org/status/500")
+  assertEquals(res.code, 500)
+  assertNotEquals(res.body, nil)
+  assertNotEquals(res.headers, nil)
+  assertEquals(res.status_line, "HTTP/1.1 500 INTERNAL SERVER ERROR")
+  -- We don't care what the error IS just that it sets err
+  assertNotEquals(res.err, nil)
+end
+
+function TestHttpClientLuaSocket:test_redir_loop()
+  -- no way to test this yet
+  -- httpbin /redirect/:n doesn't send you to same location each time
+end
+
+function TestHttpClientLuaSocket:test_missing_url()
+  local hc = httpclient.new()
+  local res = hc:get(nil)
+  assertNil(res.body)
+  assertEquals(res.err, "missing url")
+end
+
+function TestHttpClientLuaSocket:test_body()
+  local hc = httpclient.new()
+  local res = hc:post("http://httpbin.org/post",nil)
+  assertNil(res.body)
+  assertEquals(res.err, "missing data")
+end
+
+lu = LuaUnit.new()
+lu:setOutputType("tap")
+os.exit(lu:runSuite())
